073c1629dc2ac6ba76b05289d94aa2a58363838c
Antonio Yu <javeriana.yu@gmail.com>
2018-10-18 16:46:35 -0500

    Modified the read queries.

--- Shopping_Site_read.sql
+++ Shopping_Site_read.sql
@@ -105,3 +105,81 @@ exception
 when others then
 	dbms_output.put_line('There has been an error when reading the product type.');
 end productType_read;
+
+create or replace procedure errandState_read(id integer) is
+cursor temp_cursor is
+select * from errand_state where eStateID = id;
+begin
+	for read in temp_cursor loop
+		dbms_output.put_line('Errand state ID: ' || read.eStateID || chr(10) ||
+		 					 'Errand state name: ' || read.eStateName);
+	end loop;
+exception
+when others then
+	dbms_output.put_line('There has been an error when reading the errand state.');
+end errandState_read;
+
+create or replace procedure provider_read(id integer) is
+cursor temp_cursor is
+select * from provider where providerID = id;
+begin
+	for read in temp_cursor loop
+		dbms_output.put_line('Provider ID: ' || read.providerID || chr(10) ||
+		 					 'Provider name: ' || read.providerName || chr(10) ||
+		 					 'Provider email: ' || read.providerEmail || chr(10) ||
+		 					 'Provider phone: ' || read.providerPhone || chr(10) ||
+		 					 'Provider address: ' || read.providerAddress || chr(10) ||
+		 					 'City ID: ' || read.cityID);
+	end loop;
+exception
+when others then
+	dbms_output.put_line('There has been an error when reading the provider.');
+end provider_read;
+
+create or replace procedure product_read(id integer) is
+cursor temp_cursor is
+select * from product where productID = id;
+begin
+	for read in temp_cursor loop
+		dbms_output.put_line('Product ID: ' || read.productID || chr(10) ||
+		 					 'Product name: ' || read.productName || chr(10) ||
+		 					 'Product price: ' || read.productPrice || chr(10) ||
+		 					 'Product VAT: ' || read.productIVA || '%' ||chr(10) ||
+		 					 'Product quantity: ' || read.productQuantity || chr(10) ||
+		 					 'Product weight: ' || read.productWeight || chr(10) ||
+		 					 'Product state: ' || read.productState || chr(10) ||
+		 					 'Category ID: ' || read.categoryID || chr(10) ||
+		 					 'Provider ID: ' || read.providerID || chr(10) ||
+		 					 'Envelope ID: ' || read.envelopeID);
+	end loop;
+exception
+when others then
+	dbms_output.put_line('There has been an error when reading the product.');
+end product_read;
+
+create or replace procedure client_read(id integer) is
+cursor temp_cursor is
+select * from client where clientID = id;
+begin
+	for read in temp_cursor loop
+		dbms_output.put_line('Client ID: ' || read.clientID || chr(10) ||
+		 					 'Client name: ' || read.clientName || chr(10) ||
+		 					 'Client email: ' || read.clientEmail || chr(10) ||
+		 					 'Client phone: ' || read.clientPhone || chr(10) ||
+		 					 'Client address: ' || read.clientAddress || chr(10) ||
+		 					 'City ID: ' || read.cityID || chr(10) ||
+		 					 'Client entity ID: ' || read.cEntityID || chr(10) ||
+		 					 'Client type ID: ' || read.cTypeID);
+	end loop;
+exception
+when others then
+	dbms_output.put_line('There has been an error when reading the client.');
+end client_read;
+
+create or replace procedure catalogue_read(id integer) is
+cursor temp_cursor is
+select productName
+from catalogue_detail inner join product on catalogue_detail.productID = product.productID
+where catalogue_detail.catalogueID = id;
+begin
+end catalogue_read;
